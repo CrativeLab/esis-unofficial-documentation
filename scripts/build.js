0e2ffa4e62c464be7d34334a9ed9d9c546d79582
__filename <filename@inft.kr>
2024-09-29 17:30:22 +0800

    ci(pages): build setup

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,13 @@ import { $ } from 'zx';
 await $`tsp compile src`;
 await $`rm -rf build`;
 await $`mkdir build`;
-await $`mkdir build/unofficial-documentation`;
-await $`cp favicon.svg build/unofficial-documentation`;
-await $`cp index.html build/unofficial-documentation`;
-await $`cp -r tsp-output build/unofficial-documentation`;
-await $`cp -r vendors build/unofficial-documentation`;
-await $`cp -r assets build/unofficial-documentation`;
-await $`echo '<meta http-equiv="refresh" content="0; url=/unofficial-documentation" />' >build/index.html`;
+await $`mkdir build/esis-unofficial-documentation`;
+await $`cp favicon.svg build/esis-unofficial-documentation`;
+await $`cp index.html build/esis-unofficial-documentation`;
+await $`cp -r tsp-output build/esis-unofficial-documentation`;
+await $`cp -r vendors build/esis-unofficial-documentation`;
+await $`cp -r assets build/esis-unofficial-documentation`;
+await $`echo '<meta http-equiv="refresh" content="0; url=/esis-unofficial-documentation" />' >build/index.html`;
 
 const branch = await $`git branch --show-current`.quiet();
 if (branch.stdout.trim() != 'main') {
@@ -17,5 +17,5 @@ if (branch.stdout.trim() != 'main') {
   const version = `${hash}`;
 
   $.quote = (s) => s;
-  await $`sed -E "s/version:.+/version: \\"${version}\\"/" -i "build/unofficial-documentation/tsp-output/@typespec/openapi3/openapi.yaml"`;
+  await $`sed -E "s/version:.+/version: \\"${version}\\"/" -i "build/esis-unofficial-documentation/tsp-output/@typespec/openapi3/openapi.yaml"`;
 }
