33f730848e13a416829b9da19dfba42d745af78f
__filename <filename@inft.kr>
2024-09-29 17:38:09 +0800

    feat: added html page

--- scripts/build.js
+++ scripts/build.js
@@ -4,11 +4,8 @@ await $`tsp compile src`;
 await $`rm -rf build`;
 await $`mkdir build`;
 await $`mkdir build/esis-unofficial-documentation`;
-await $`cp favicon.svg build/esis-unofficial-documentation`;
 await $`cp index.html build/esis-unofficial-documentation`;
 await $`cp -r tsp-output build/esis-unofficial-documentation`;
-await $`cp -r vendors build/esis-unofficial-documentation`;
-await $`cp -r assets build/esis-unofficial-documentation`;
 await $`echo '<meta http-equiv="refresh" content="0; url=/esis-unofficial-documentation" />' >build/index.html`;
 
 const branch = await $`git branch --show-current`.quiet();
